5fa79ba0a8c7987ed338aa3d495fe851c890a14d
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-08 15:58:12 -0400

    reads csvs into 2-level dict now

--- python/transfer_finder.py
+++ python/transfer_finder.py
@@ -5,20 +5,29 @@ import csv
 def load_transfer_points():
     input_filename = "../data/Transfer_Points.csv"
 
+    transfer_points = {}
+
     with open(input_filename, 'rU') as input_csv:
         reader = csv.DictReader(input_csv, dialect="excel")
-        transfer_points = [row for row in reader]
+        for row in reader:
+            station = row.pop('Station')
+            transfer_points[station] = row
+
     return transfer_points
 
 def load_line_positions():
     input_filename = "../data/Line_Positions.csv"
 
+    line_positions = {}
     with open(input_filename, 'rU') as input_csv:
         reader = csv.DictReader(input_csv, dialect="excel")
-        line_positions = [row for row in reader]
+        for row in reader:
+            station = row.pop('Station')
+            line_positions[station] = row
+
     return line_positions
 
 transfer_points = load_transfer_points()
-print transfer_points[0]
+print transfer_points['Metro Center']
 line_positions = load_line_positions()
-# print line_positions[0]
+print line_positions['Metro Center']
